02a9938812dd29ad54b82a8dda7d67f02d784a7f
pedroafonsoo <carlospmafonso@hotmail.com>
2015-11-21 15:43:38 +0000

    Exercicio 45 da ficha 1

---
+++ Ficha5/ex45ficha1.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+ int n_divisores (int n){
+
+    int c,i,x;
+    c=0,i=1;
+
+    while(i<=n){
+
+        if(n%i==0){
+
+                c++;
+            i++;
+        }
+
+        return c;
+    }
+
+
+ }
+
+
+
+
+
+
+void main(){
+
+    int n,x;
+
+
+        do{
+
+        printf("Introduza um numero \n");
+        scanf("%d",&n);
+
+        if(n>0){
+        x=n_divisores(n);
+
+        if(x==2 || x==1){
+            printf("Entao e numero primo \n");
+
+        }
+
+    }while(n>0);
+
+
+
+
+
+
+
+}
