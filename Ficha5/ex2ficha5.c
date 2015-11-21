015219f823fc41f4b87c1c6a3a7f95a616cb62a5
pedroafonsoo <carlospmafonso@hotmail.com>
2015-11-21 11:36:05 +0000

    Leitura dinâmica de valores para a ficha 5 ,exercicio 2

--- Ficha5/ex2ficha5.c
+++ Ficha5/ex2ficha5.c
@@ -11,10 +11,13 @@
  }
 
 void main(){
-
-    int n = 3;
-    int min= 0;
-    int max=4;
+    int n,min,max;
+    printf("Introduza um numero \n");
+    scanf("%d",&n);
+    printf("Introduza o limite minimo \n");
+    scanf("%d",&min);
+    printf("Introduza o limite maximo\n");
+    scanf("%d",&max);
     int resultado= n_dentro_limite(n,min,max); //   A ordem dos argumentos tem que ser igual á da função
     printf("Resultado:%d \n",resultado);
 
